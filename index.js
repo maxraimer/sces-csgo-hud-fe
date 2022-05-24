8b5b8f8963297818def00b1e21431d542e6e2ef2
Maksym Bukhan <maksym.bukhan@gmail.com>
2022-05-25 01:35:59 +0300

    Update v0.6
    Optimised, added few features

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 http.globalAgent.maxSockets = Infinity;
+app.use(express.static('public'));
 
 let data = '';
 let connections = [];
